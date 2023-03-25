c5c5a9d1bb389fb050f89c46ba2e4016bcb58c5a
Abandi0122 <acsaiandras@gmail.com>
2023-03-25 17:03:44 +0100

    IOC

--- studmon/studmonBackend/Program.cs
+++ studmon/studmonBackend/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using studmon.Models;
 using studmonBackend.Data.DBContext;
+using studmonBackend.Data.Repositories;
+using studmonBackend.Logic;
+using studmonBackend.Logic.Interfaces;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,9 +28,26 @@ builder.Services.AddIdentity<Tanar, IdentityRole>(option =>
 .AddEntityFrameworkStores<ApplicationDBContext>()
 .AddDefaultTokenProviders();
 
+///////////////////
+builder.Services.AddTransient<ApplicationDBContext>();
+///////////////////
+builder.Services.AddTransient<IRepository<Terem>, TeremRepository>();
+builder.Services.AddTransient<IRepository<Ora>, OraRepository>();
+builder.Services.AddTransient<IRepository<Tanar>, TanarRepository>();
+builder.Services.AddTransient<IRepository<Hallgato>, HallgatoRepository>();
+builder.Services.AddTransient<IRepository<Teljesitmeny>, TeljesitmenyRepository>();
+///////////////////
+builder.Services.AddTransient<ITeremLogic, TeremLogic>();
+builder.Services.AddTransient<IOraLogic, OraLogic>();
+builder.Services.AddTransient<ITanarLogic, TanarLogic>();
+builder.Services.AddTransient<IHallgatoLogic, HallgatoLogic>();
+builder.Services.AddTransient<ITeljesitmenyLogic, TeljesitmenyLogic>();
+///////////////////
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
