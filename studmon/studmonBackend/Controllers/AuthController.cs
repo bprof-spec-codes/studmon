2ae7afea6767fc76d5bcc8479d448b9dd2da7170
Abandi0122 <acsaiandras@gmail.com>
2023-04-05 19:18:06 +0200

    Auth elvileg működik

---
+++ studmon/studmonBackend/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using studmonBackend.Data.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace studmonBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<Tanar> _userManager;
+        //private readonly Microsoft.Extensions.Configuration.IConfiguration _configuration;
+        public AuthController(UserManager<Tanar> userManager)
+        {
+            _userManager = userManager;
+            //_configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claim = new List<Claim> { new Claim(JwtRegisteredClaimNames.Sub, user.UserName) };
+                foreach (var role in await _userManager.GetRolesAsync(user))
+                {
+                    claim.Add(new Claim(ClaimTypes.Role, role));
+                }
+                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("nagyonhosszutitkoskodhelye"));
+                var token = new JwtSecurityToken(
+                issuer: "http://www.security.org", audience: "http://www.security.org",
+                claims: claim, expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
