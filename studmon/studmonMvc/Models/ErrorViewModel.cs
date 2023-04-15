07cbc0abda4a2e5b4b4059be8c5ee9ac58974a50
il7n9h <il7n9h@stud.uni-obuda.hu>
2023-04-15 15:52:24 +0200

    Created

---
+++ studmon/studmonMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace studmonMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
