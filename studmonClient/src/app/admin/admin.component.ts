635cb80f7c7f30fea6b079e48bc259cfe2e0c97b
il7n9h <il7n9h@stud.uni-obuda.hu>
2023-05-15 20:23:30 +0200

    site kinezete

--- studmonClient/src/app/admin/admin.component.ts
+++ studmonClient/src/app/admin/admin.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Tanar } from '../_models/tanar';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
+
 export class AdminComponent {
+  http: HttpClient
+  teacher: Tanar
+
+  constructor(http: HttpClient) {
+    this.http = http
+    this.teacher = new Tanar()
+  }
+
+  public createTeacher() : void {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('studmon-token')
+    })
+    this.http
+      .post(
+        'http://localhost:5231/TanarAPI',
+        this.teacher,
+        { headers: headers }
+      )
+  }
+
+  
+
 
 }
