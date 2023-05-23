88b8eeceb83ec670eee1738f383f870f73e56fc7
Borossa Zoltán <b.zoltan.2001@gmail.com>
2023-05-23 18:52:32 +0200

    Main-page kinézet endpointokkal

---
+++ studmonClient/src/app/_models/oraModel.ts
@@ -0,0 +1,11 @@
+import { TeremModel } from "./teremModel"
+
+export class Ora {
+  public id: string = ''
+  public nev: string = ''
+  public leiras: string = ''
+  public teremNev: string = ''
+  public oraKezdet: Date = new Date()
+  public oraVeg: Date = new Date()
+  public alkalmakSzama: number[] = []
+}
