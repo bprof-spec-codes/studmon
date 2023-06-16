0709aacb2e73ed24db69fee1ffb8d168bc0f9150
Borossa Zoltán <b.zoltan.2001@gmail.com>
2023-06-16 17:06:37 +0200

    Alertek és javítások

--- studmonClient/src/app/admin-modify/admin-modify.component.ts
+++ studmonClient/src/app/admin-modify/admin-modify.component.ts
@@ -23,70 +23,70 @@ export class AdminModifyComponent {
   api: ApiService;
   selectedObject: string
 
-  constructor(http: HttpClient, api: ApiService, router: Router){
+  constructor(http: HttpClient, api: ApiService, router: Router) {
     this.http = http;
     this.subjects = [];
-    this.classrooms =[];
-    this.students =[];
+    this.classrooms = [];
+    this.students = [];
     this.router = router;
     this.api = api;
     this.selectedObject = 'Hallgato';
 
   }
 
-  ngOnInit() : void{
+  ngOnInit(): void {
     this.http
-    .get<Array<studentModel>>('http://localhost:5231/HallgatoAPI')
-    .subscribe(resp=> {
-      resp.map(x=>{
-        let s = new studentModel();
-        s.neptunKod = x.neptunKod;
-        s.nev = x.nev;
-        s.kar = x.kar;
-        s.kepzesNev = x.kepzesNev;
-        s.orak = x.orak;
-        s.teljesitmeny = x.teljesitmeny;
-        this.students.push(s);
+      .get<Array<studentModel>>('http://localhost:5231/HallgatoAPI')
+      .subscribe(resp => {
+        resp.map(x => {
+          let s = new studentModel();
+          s.neptunKod = x.neptunKod;
+          s.nev = x.nev;
+          s.kar = x.kar;
+          s.kepzesNev = x.kepzesNev;
+          s.orak = x.orak;
+          s.teljesitmeny = x.teljesitmeny;
+          this.students.push(s);
+        })
+        console.log(this.students)
       })
-      console.log(this.students)
-    })
 
     this.http
-    .get<Array<TeremModel>>('http://localhost:5231/TeremAPI')
-    .subscribe(resp=> {
-      resp.map(x=>{
-        let t = new TeremModel();
-        t.nev = x.nev;
-        t.elrendezes = x.elrendezes;
-        t.orak = x.orak;
-        this.classrooms.push(t);
+      .get<Array<TeremModel>>('http://localhost:5231/TeremAPI')
+      .subscribe(resp => {
+        resp.map(x => {
+          let t = new TeremModel();
+          t.nev = x.nev;
+          t.elrendezes = x.elrendezes;
+          t.orak = x.orak;
+          this.classrooms.push(t);
+        })
+        console.log(this.classrooms)
       })
-      console.log(this.classrooms)
-    })
 
     this.http
-    .get<Array<Ora>>('http://localhost:5231/OraAPI')
-    .subscribe(resp=> {
-      console.log("RESP",resp)
-      resp.map((x:any)=>{
-        let o = new Ora();
-        o.id = x.id;
-        o.nev = x.nev;
-        o.leiras = x.leiras;
-        o.teremNev = x.teremID;
-        o.oraKezdet = x.oraKezdet;
-        o.oraVeg = x.oraVeg;
-        o.alkalmakSzama = x.alkalmakSzama;
-        o.selectedAlkalom = x.selectedAlkalom;
-        this.subjects.push(o);
+      .get<Array<Ora>>('http://localhost:5231/OraAPI')
+      .subscribe(resp => {
+        console.log("RESP", resp)
+        resp.map((x: any) => {
+          let o = new Ora();
+          o.id = x.id;
+          o.nev = x.nev;
+          o.leiras = x.leiras;
+          o.teremNev = x.teremID;
+          o.oraKezdet = x.oraKezdet;
+          o.oraVeg = x.oraVeg;
+          o.alkalmakSzama = x.alkalmakSzama;
+          o.selectedAlkalom = x.selectedAlkalom;
+          this.subjects.push(o);
+        })
+        console.log(this.subjects)
       })
-      console.log(this.subjects)
-    })
 
 
   }
 
-  public delete(tipus: string, id: string) : void {
+  public delete(tipus: string, id: string): void {
     const baseUrl = 'http://localhost:5231/';
     let headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -94,16 +94,35 @@ export class AdminModifyComponent {
     })
     this.http
       .delete(
-        `${baseUrl}${tipus}API/`+ id,
+        `${baseUrl}${tipus}API/` + id,
         { headers: headers }
       )
       .subscribe(
         (success) => {
           //this.snackBar.open("Delete successful!", "Close", { duration: 5000 })
           console.log(success)
-          this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          let i
+          switch (tipus) {
+            case "Hallgato": {
+              i = this.students.findIndex((t: any) => t.neptunKod === id);
+              this.students.splice(i,1)
+              break;
+            }
+            case "Ora": {
+              i = this.subjects.findIndex((t: any) => t.id === id);
+              this.subjects.splice(i,1)
+              break;
+            }
+            case "Terem": {
+              i = this.classrooms.findIndex((t: any) => t.nev === id);
+              this.classrooms.splice(i,1)
+              break;
+            }
+          }
+          alert("Sikeres törlés!")
+          /*this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
             this.router.navigate(['/admin-modify']);
-          });
+          });*/
         },
         (error) => {
           console.log(error)
@@ -113,7 +132,7 @@ export class AdminModifyComponent {
 
   }
 
-  public SelectObject(type: string){
+  public SelectObject(type: string) {
     this.selectedObject = type;
     console.log(this.selectedObject)
   }
